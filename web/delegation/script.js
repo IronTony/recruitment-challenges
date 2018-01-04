a086abcb5549b1bab6a9f23a14bc5856d65872a1
Francesco Giucastro <fragiucas@gmail.com>
2018-01-04 22:43:36 +0100

    Delegation Exercise

--- web/delegation/script.js
+++ web/delegation/script.js
@@ -1,1 +1,11 @@
-// Your code here
+function onClicking() {
+  var list = document.querySelectorAll('li a');
+
+  Array.prototype.slice.call(list).forEach((item) => {
+    item.addEventListener('click', (e) => {
+      alert(e.target.innerHTML)
+    });
+  });
+}
+
+window.onload = onClicking
