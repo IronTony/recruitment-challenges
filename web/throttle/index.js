c982cfb6164d8f52d7889b5afefce8a5eadbfffb
Francesco Giucastro <fragiucas@gmail.com>
2018-01-05 09:55:34 +0100

    Throttle exercise

--- web/throttle/index.js
+++ web/throttle/index.js
@@ -1,1 +1,30 @@
-// Your code here
+const throttlePromises = async (limit, promises) => {
+  let results = []
+  let i = 0
+  let promisesWorking = 0
+
+  const resolveNextPromise = () => {
+    if (!promises.length) return
+
+    const promise = promises.pop()
+    promisesWorking++
+
+    if (promisesWorking < limit) {
+      resolveNextPromise()
+    }
+
+    return promise().then((res) => {
+      results[i] = res
+      i++
+      promisesWorking--
+
+      return resolveNextPromise()
+    })
+  }
+
+  await resolveNextPromise()
+
+  return results
+}
+
+module.exports = throttlePromises
